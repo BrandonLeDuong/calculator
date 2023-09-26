a08a5e94dfe55aa2ce3c04575a7e46d7cfb6da2a
Brandon Duong <bduong1497@gmail.com>
2023-09-25 22:36:48 -0400

    Added basic arithmetic functions: addNumbers, subtractNumbers, multiplyNumbers, and divideNumbers

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function addNumbers(a, b) {
+  let result = a + b;
+  return result;
+}
+
+function subtractNumbers(a, b) {
+  let result = a - b;
+  return result;
+}
+
+function multiplyNumbers(a, b) {
+  let result = a * b;
+  return result;
+}
+
+function divideNumbers(a, b) {
+  let result = a / b;
+  return result;
+}
